85dbc11704f272dceba62ae4282afce39ca9e46a
Jacob Harrington <jharring@usc.edu>
2020-09-13 15:29:02 -0700

    reverse.cpp complete

--- reverse.cpp
+++ reverse.cpp
@@ -3,16 +3,21 @@
 
 using namespace std;
 
-//change this function so that it reverses 'str' recursively, rather than iteratively.
-//you will probably want to add a function parameter.
-//you may not use while, for, do while, or goto.
-
-string reverse(string str) {
-    string ans = str;
-    for (unsigned int i = 0; i < str.length()/2; i++) {
-        swap(ans[i], ans[str.length()-i-1]);
+string reverse(string str){
+    // base case 1: if string is 0/1 char, do nothing
+    if( str.length() <= 1 ) return str;
+    
+    // base case 2: if string is 2 chars, swap & return
+    else if( str.length() == 2) {
+        swap(str[0], str[1]);
+        return str;
     }
-    return ans;
+    
+    // make the problem smaller
+    string sub_str = str.substr(1);
+    
+    // widdle the string through R.C.s down to base case 2, then concatenate the char prior to substring start onto the end of the returned string
+    return reverse( sub_str ) + str[0];
 }
 
 int main(int argc, char* argv[]) {
